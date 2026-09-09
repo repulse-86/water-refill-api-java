8a97db495a36c5d0a606bf2ac9944c2b54038f53
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2026-09-09 17:20:53 +0800

    chore: set up Spring Boot project

---
+++ src/main/java/com/example/waterrefillapijava/WaterRefillApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.waterrefillapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterRefillApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterRefillApiJavaApplication.class, args);
+	}
+
+}
